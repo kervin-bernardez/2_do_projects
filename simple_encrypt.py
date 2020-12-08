a2b89c1df393ef4d88017b20b26a5234162c9bf5
Mark Bernardez <markbernardez@MacBook-Pro.local>
2020-12-08 17:45:24 +0800

    merge

--- simple_encrypt.py
+++ simple_encrypt.py
@@ -29,33 +29,14 @@ def encode(texts, shift):
         else:
             ntext.append(dicts[text])
     return "".join(ntext)
-<<<<<<< Updated upstream
 
-#enter inputs
-text = input("Enter filename:\n" ) #e.g. test.txt
-shift = int(input("Enter the number of places you want to shift:\n" )) #e.g. -2
+
+# enter inputs
+text = input("Enter filename:\n")  # e.g. test.txt
+shift = int(input("Enter the number of places you want to shift:\n"))  # e.g. -2
 #texts = "Hello World A123456 @{* Ωñæ"
 
-#open file
+# open file
 with open(text, 'r') as filename:
     texts = filename.read().replace('\n', ' ')
 print(encode(texts, shift))
-
-
-#shift = -2
-#texts = "Hello World A123456 @{* Ωñæ"
-#print(encode(texts, shift))
-
-"""
-ISSUES TO BE SOLVED:
-Make Test code
-Test code
-Make a decrpytor
-Clean code
-Break code
-Optimize code
-Open file to encrypt
-Test more 
-"""
-=======
->>>>>>> Stashed changes
