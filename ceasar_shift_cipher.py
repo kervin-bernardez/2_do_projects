2adbf3e4d92a44dbe227284fd7cb45eb7f8bf992
Mark Bernardez <markbernardez@MacBook-Pro.local>
2020-12-10 21:22:53 +0800

    code made to run faster

--- ceasar_shift_cipher.py
+++ ceasar_shift_cipher.py
@@ -8,12 +8,26 @@ def shift_list(paramater, shift):
     return shifted
 
 
-def encode(texts, shift):
+def encode(texts, dicts):
+    # replace words
+    ntext = []
+    keys = dicts.keys()
+    for text in texts:
+        if text not in keys:
+            ntext.append(text)
+        else:
+            ntext.append(dicts[text])
+    return "".join(ntext)
+
+
+def main():
+    # enter inputs
+    input_file = input("Enter filename:\n")
+    shift = input(int("Enter the number of places you want to shift:\n"))
     # var init
     letters = "abcdefghijklmnopqrstuvwxyz"
     numbers = "0123456789"
     symbols = "[]{}()+-*/:;<>,.?!_@#"
-    shift = int(shift)
     # make shifted list
     s_letters = shift_list(letters, shift) + shift_list(letters.upper(), shift)
     s_numbers = shift_list(numbers, shift)
@@ -21,28 +35,14 @@ def encode(texts, shift):
     # make shifted dictionary
     dicts = dict(zip(letters+letters.upper()+numbers+symbols,
                      s_letters+s_numbers+s_symbols))
-    # replace words
-    ntext = []
-    for text in texts:
-        if text not in dicts.keys():
-            ntext.append(text)
-        else:
-            ntext.append(dicts[text])
-    return "".join(ntext)
-
-
-def run():
-    # enter inputs
-    input_file = input("Enter filename:\n")
-    input_shift = input("Enter the number of places you want to shift:\n")
-    coded = []
     # open file
+    coded = []
     with open(input_file, "r", encoding='utf-8', errors='replace') as filename:
         texts = filename.read().splitlines()
         for text in texts:
-            code = encode(text, input_shift)
+            code = encode(text, dicts)
             coded.append(code)
-    # rewrite file
+    # write file
     with open(input_file, "w", encoding='utf-8', errors='replace') as filename:
         for code in coded:
             filename.write(code + "\n")
