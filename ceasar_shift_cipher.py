e130c663c0df52129ce5e263ad0a1be674e46aca
Mark Bernardez <markbernardez@MacBook-Pro.local>
2020-12-13 00:53:29 +0800

    separated var feature

--- ceasar_shift_cipher.py
+++ ceasar_shift_cipher.py
@@ -8,6 +8,21 @@ def shift_list(paramater, shift):
     return shifted
 
 
+def var(shift):
+    # var init
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    numbers = "0123456789"
+    symbols = "[]{}()+-*/:;<>,.?!_@#"
+    # make shifted list
+    s_letters = shift_list(letters, shift) + shift_list(letters.upper(), shift)
+    s_numbers = shift_list(numbers, shift)
+    s_symbols = shift_list(symbols, shift)
+    # make shifted dictionary
+    dicts = dict(zip(letters+letters.upper()+numbers+symbols,
+                     s_letters+s_numbers+s_symbols))
+    return dicts
+
+
 def encode(texts, dicts):
     # replace words
     ntext = []
@@ -23,20 +38,10 @@ def encode(texts, dicts):
 def main():
     # enter inputs
     input_file = input("Enter filename:\n")
-    shift = input(int("Enter the number of places you want to shift:\n"))
-    # var init
-    letters = "abcdefghijklmnopqrstuvwxyz"
-    numbers = "0123456789"
-    symbols = "[]{}()+-*/:;<>,.?!_@#"
-    # make shifted list
-    s_letters = shift_list(letters, shift) + shift_list(letters.upper(), shift)
-    s_numbers = shift_list(numbers, shift)
-    s_symbols = shift_list(symbols, shift)
-    # make shifted dictionary
-    dicts = dict(zip(letters+letters.upper()+numbers+symbols,
-                     s_letters+s_numbers+s_symbols))
-    # open file
+    shift = int(input("Enter the number of places you want to shift:\n"))
+    dicts = var(shift)
     coded = []
+    # open file
     with open(input_file, "r", encoding='utf-8', errors='replace') as filename:
         texts = filename.read().splitlines()
         for text in texts:
