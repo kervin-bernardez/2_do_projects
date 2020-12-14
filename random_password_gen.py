b93339d3b1eae848aa09cbc63d04aa0a4ee770a4
Mark Bernardez <markbernardez@MacBook-Pro.local>
2020-12-14 16:21:05 +0800

    added *args/changed sample to choice

--- random_password_gen.py
+++ random_password_gen.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
-
 import random
 
 
-def generate():
+def generate(*args):
+    # var init
     letters = "abcdefghijklmnopqrstuvwxyz"
     numb_symbs = "0123456789[]{}()*;/,._-"
-
     all = letters + letters.upper() + numb_symbs
-
-    length = 16
-    password = "".join(random.sample(all, length))
+    # check for *args else go to default lenght
+    if len(args) != 0:
+        length = args[0]
+    else:
+        length = 16
+    # generate and returns password
+    password = ''.join([random.choice(all) for _ in range(length)])
     return(password)
 
 
