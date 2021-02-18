dcbd6f880eb5cc1adaba68546a7789d9f48e7267
Mark Bernardez <markbernardez@MacBook-Pro.local>
2021-02-18 14:55:45 +0800

    added sha512

--- hash_check.py
+++ hash_check.py
@@ -5,7 +5,7 @@ def hasher(texts, *args):
     if len(args) != 0:
         hash_algo = [args[0]]
     else:
-        hash_algo = ["md5", "sha1", "sha256"]
+        hash_algo = ["md5", "sha1", "sha256", "sha512"]
     hash_list = []
     for algo in hash_algo:
         if algo == "md5":
@@ -14,6 +14,8 @@ def hasher(texts, *args):
             hash = hashlib.sha1(texts)
         elif algo == "sha256":
             hash = hashlib.sha256(texts)
+        elif algo == "sha512":
+            hash = hashlib.sha512(texts)
         else:
             return False
         hash_list.append(hash.hexdigest())
